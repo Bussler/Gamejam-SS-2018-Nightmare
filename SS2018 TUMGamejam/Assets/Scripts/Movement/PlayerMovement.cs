1e77fde9b397af8b798f9ec591eb1dfae836b894
Joshua Weggartner <Joshua.Weggartner@gmx.de>
2018-06-17 01:26:49 +0200

    sound walking

--- SS2018 TUMGamejam/Assets/Scripts/Movement/PlayerMovement.cs
+++ SS2018 TUMGamejam/Assets/Scripts/Movement/PlayerMovement.cs
@@ -8,8 +8,8 @@ public class PlayerMovement : MonoBehaviour { //This script experiments with mov
     CharacterController controller; //character controller
     public float rotateSpeed=60;
     public float moveSpeed=10;
-    public float inAirSpeed = 3;
-    public float jumpSpeed=12;
+    public float inAirSpeed = 10;
+    public float jumpSpeed=0;
     public float gravity = 20;
 
     private float lastYSpeed = 0;//need this for in air movement
@@ -19,6 +19,7 @@ public class PlayerMovement : MonoBehaviour { //This script experiments with mov
     private bool wasjumping;
     private float originalMoveSpeed;
     private float helperSpeed;
+    private AudioSource audio;
     
 
     Vector3 moveValues = Vector3.zero;
@@ -29,6 +30,7 @@ public class PlayerMovement : MonoBehaviour { //This script experiments with mov
         controller = GetComponent<CharacterController>();
         originalMoveSpeed = moveSpeed;
         helperSpeed = moveSpeed / 2;
+        audio = this.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -70,7 +72,7 @@ public class PlayerMovement : MonoBehaviour { //This script experiments with mov
 
             moveValues = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, 0.0f, Input.GetAxis("Vertical")*moveSpeed);
             moveValues = transform.TransformDirection(moveValues); //transforms vector from world into local space, therefore making the Rotatation of the player count
-
+            audio.Play();
             if (Input.GetButton("Jump")&&cooldown<=0)
             {
                 moveValues.y = jumpSpeed;//making the player jump
