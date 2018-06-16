1a8628bc50c7f4f57267bd9ed3a98dca0a3d7f6f
Joshua Weggartner <Joshua.Weggartner@gmx.de>
2018-06-16 14:55:29 +0200

    Camera,title,fog

---
+++ SS2018 TUMGamejam/Assets/Scripts/StartMenu/MenuFotoSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFotoSpawner : MonoBehaviour {
+
+    public GameObject[] foto;
+    public float maxTime;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+        int x = Random.Range(0, foto.Length);
+        Instantiate(foto[x], new Vector3(Random.Range(-30, 30), 40, this.transform.position.z),foto[x].transform.rotation );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time = time + Time.deltaTime;
+        if(time>= maxTime){
+            time = 0;
+            int x = Random.Range(0, foto.Length);
+            Instantiate(foto[x], new Vector3(Random.Range(-30, 30), 40, this.transform.position.z), foto[x].transform.rotation);
+
+        }
+		
+	}
+}
---
+++ SS2018 TUMGamejam/Assets/Scripts/StartMenu/SelectLevelSize.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SelectLevelSize : MonoBehaviour {
+
+    public Dropdown dropdown;
+    public int smallSize;
+    public int mediumSize;
+    public int bigSize;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeLevelSize()
+    {
+        switch (dropdown.value)
+        {
+            case 0:
+                Global.size = smallSize;
+                break;
+                ;
+            case 1:
+                Global.size = mediumSize;
+                break;
+                ;
+            case 2:
+                Global.size = bigSize;
+                break;
+                ;
+
+        }
+        
+
+    }
+}
