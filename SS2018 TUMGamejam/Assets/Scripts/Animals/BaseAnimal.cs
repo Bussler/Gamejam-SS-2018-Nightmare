723f2d025238db1b3952f07f870df5d129d8b236
Neroware <neroware@freenet.de>
2018-06-16 02:27:09 +0200

    Added a basic Animal AI structure

---
+++ SS2018 TUMGamejam/Assets/Scripts/Animals/BaseAnimal.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseAnimal : MonoBehaviour {
+
+	private AnimalState LAST_STATE = AnimalState.none;
+	private AnimalState CURRENT_STATE = AnimalState.none;
+	private AnimalState NEXT_STATE = AnimalState.walking;
+
+	private NavMeshAgent navAgent;
+
+	private float standingTimer = 0.0f;
+	private Boolean rotating = false;
+
+	public enum AnimalState
+	{
+		none,
+		walking,
+		standing,
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<NavMeshAgent>();
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		LAST_STATE = CURRENT_STATE;
+		CURRENT_STATE = NEXT_STATE;
+
+		Behavior();
+	}
+
+
+	private void Behavior()
+	{
+		switch (CURRENT_STATE)
+		{
+			case AnimalState.walking:
+				{
+					navAgent.SetDestination(new Vector3(4, 4, 4));
+					navAgent.isStopped = false;
+					if(navAgent.remainingDistance < 0.5f)
+					{
+						NEXT_STATE = AnimalState.standing;
+					}
+					break;
+				}
+			case AnimalState.standing:
+				{
+					System.Random r = new System.Random();
+
+					if(standingTimer >= 4.5f)
+					{
+						rotating = true;
+					}
+
+					if (rotating)
+					{
+						RotateToPlayer();
+					}
+
+					standingTimer += Time.deltaTime;
+					if(standingTimer > 30.0f)
+					{
+						standingTimer = 0.0f;
+						rotating = false;
+						NEXT_STATE = AnimalState.none;
+					}
+
+					break;
+				}
+		}
+	}
+
+
+	public void RotateToPlayer()
+	{
+		Quaternion lookRotation;
+		Vector3 direction;
+
+		//find the vector pointing from our position to the target
+		direction = (new Vector3(0, 0, 0) - transform.position).normalized;
+
+		//create the rotation we need to be in to look at the target
+		lookRotation = Quaternion.LookRotation(direction);
+
+		//rotate us over time according to speed until we are in the required rotation
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 0.5f);
+	}
+}
